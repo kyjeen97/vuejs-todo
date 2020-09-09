608cb8a3ac0cb4100ae583040ea800e2be997e0c
Kok Yu Jeen <kyjeen97@gmail.com>
2020-09-09 17:42:43 +0800

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+            <input type="text" name="todoInput" id="todoInput" placeholder="What to do?" />
+            <button class="submit" id="createTodo">+</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+style with <style lang="scss" scoped>
+    header {
+        display: flex;
+        input {
+            flex: 1;
+            height: 35px;
+            box-sizing: border-box;
+        }
+
+        .submit {
+            width: 50px;
+            height: 35px;
+        }
+    }
+</style>
